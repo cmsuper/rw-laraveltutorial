bb7c3ad658479cb39674379a2ab8b85e50e7492d
cmssuper <anggit@rumahweb.co.id>
2024-05-27 13:43:33 +0700

    ini adalah tutorial laravel 10

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>halaman kontak</title>
+</head>
+<body>
+ 
+	<h3>Hubungi Kami</h3>
+	<p>Ini adalah halaman kontak</p>
+	<p>Silakan hubungi kami di nomor 123456789</p>
+ 
+</body>
+</html>
